e2dd9650771da0705fc1aaaf8179bb434dc6931f
Samet Taş <bmsamettas@gmail.com>
2024-05-15 14:58:36 +0300

    home screen

--- App.js
+++ App.js
@@ -2,13 +2,12 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux'
 import { store } from './store';
+import HomeScreen from './screens/HomeScreen';
 
 export default function App() {
   return (
     <Provider store={store}>
-    <View style={styles.container}>
-      <Text>UBER</Text>
-    </View>
+    <HomeScreen></HomeScreen>
     </Provider>
   );
 }
