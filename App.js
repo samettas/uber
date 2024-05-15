e56514a5409119a0761c91f1f281ecfd9e237013
Samet Taş <bmsamettas@gmail.com>
2024-05-15 14:36:57 +0300

    setting up and implementing redux

--- App.js
+++ App.js
@@ -1,13 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from 'react-redux'
+import { store } from './store';
 
 export default function App() {
   return (
+    <Provider store={store}>
     <View style={styles.container}>
       <Text>UBER</Text>
-      <StatusBar style="auto" />
     </View>
+    </Provider>
   );
 }
 
