ac078fd53a670dbac090435100c316d5a5b18c40
Samet Taş <bmsamettas@gmail.com>
2024-05-22 00:27:07 +0300

    map component

--- screens/MapScreen.js
+++ screens/MapScreen.js
@@ -1,10 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import tw from 'twrnc';
+import Map from "../components/Map"
+import MapView from 'react-native-maps';
+import { createStackNavigator } from '@react-navigation/stack';
 
 const MapScreen = () => {
+const Stack = createStackNavigator();
+
   return (
     <View>
-      <Text>MapScreen</Text>
+      <View style={tw`h-1/2`}>
+        <Map />
+      </View>
+
+      <View style={tw`h-1/2`}>
+      <Stack.Navigator>
+        <Stack.Screen
+        name="NavigateCard"
+        component={NavigateCard}
+        options={{
+          headerShown: false,
+        }}
+        />
+      </Stack.Navigator>
+      </View>
     </View>
   )
 }
