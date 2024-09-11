fd79adb201b5a25657a2c4fff076fdfda947767c
Samet Taş <bmsamettas@gmail.com>
2024-09-11 16:54:08 +0300

    pricing

--- components/RideOptionsCard.js
+++ components/RideOptionsCard.js
@@ -28,12 +28,17 @@ const data = [
   },
 ]
 
+//PRICING
+const SURGE_CHARGE_RATE = 1.5;
+
 const RideOptionsCard = () => {
 
   const navigation = useNavigation();
   const [selected, setSelected] = useState(null);
   const travelTimeInformation = useSelector(selectTravelTimeInformation);
-  const distance = Number(travelTimeInformation.distance.value / 1000).toFixed(1);
+  //const distance = Number(travelTimeInformation.distance.value / 1000).toFixed(1);
+  //const duration = travelTimeInformation?.duration.text;
+  //console.log(duration)
   return (
     <SafeAreaView style={tw`bg-white flex-grow`}>
       <View>
@@ -46,7 +51,7 @@ const RideOptionsCard = () => {
           />
         </TouchableOpacity>
         
-        <Text style={tw`text-center py-5 text-xl`} >Select a Ride - {distance} km</Text>
+        <Text style={tw`text-center py-5 text-xl`} >Select a Ride - {Number(travelTimeInformation?.distance.value / 1000).toFixed(1)} km</Text>
       </View>
 
     <FlatList data={data}
@@ -65,13 +70,24 @@ const RideOptionsCard = () => {
           />
           <View style={tw`-ml-6`} >
             <Text style={tw`text-xl font-semibold`} >{title}</Text>
-            <Text>{travelTimeInformation.duration.text}</Text>
+            <Text>{travelTimeInformation?.duration.text}</Text>
           </View>
-          <Text style={tw`text-xl`} >99₺</Text>
+          <Text style={tw`text-xl`} >
+
+           {new Intl.NumberFormat('tr-gb',{
+            style: 'currency',
+            currency: 'TRY'
+           }).format(
+
+            (travelTimeInformation?.duration.value * SURGE_CHARGE_RATE * multiplier) / 3
+
+           ).replace('₺', '') + '₺'}
+
+          </Text>
         </TouchableOpacity>
       )}
     />
-    <View>
+    <View style={tw`mt-auto border-t border-gray-200`} >
       <TouchableOpacity disabled={!selected} style={[tw`bg-black py-3 m-3`,!selected && tw`bg-gray-300`]} >
         <Text style={tw`text-center text-white text-xl`} >Choose {selected?.title}</Text>
       </TouchableOpacity>
